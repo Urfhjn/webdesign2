bdbb753468cf86a75b6a1f2d939303a084954fe4
enchance <enchance@gmail.com>
2021-05-01 09:01:56 +0800

    Reset cache

---
+++ gitdemo/index.php
@@ -0,0 +1,18 @@
+<?php 
+$title = 'My Simple Site';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title ?></title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="nofile.css">
+</head>
+<body>
+
+    <h1><?php echo $title ?></h1>
+    <p>This site will be used with git.</p>
+    <p>Hello.</p>
+
+</body>
+</html>
